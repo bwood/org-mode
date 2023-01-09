d94440ebb901e2f8ed819edf9dae3232e5360d95
Brian <bwood@berkeley.edu>
2023-01-09 11:45:27 -0800

    Apply step-headers-off.patch.

--- lisp/org-clock.el
+++ lisp/org-clock.el
@@ -2859,6 +2859,7 @@ a number of clock tables."
             (`year "Annual report starting on: ")
             (`quarter "Quarterly report starting on: ")
             (_ (user-error "Unknown `:step' specification: %S" step))))
+         (step-headers-off (plist-get params :step-headers-off))
          (week-start (or (plist-get params :wstart) 1))
          (month-start (or (plist-get params :mstart) 1))
          (range
@@ -2888,11 +2889,12 @@ a number of clock tables."
             (timestamp (seconds-to-time (org-matcher-time timestamp))))))
     (while (time-less-p start end)
       (unless (bolp) (insert "\n"))
+      (unless step-headers-off
       ;; Insert header before each clock table.
       (insert "\n"
               step-header
               (format-time-string (org-time-stamp-format nil t) start)
-	      "\n")
+	      "\n"))
       ;; Compute NEXT, which is the end of the current clock table,
       ;; according to step.
       (let* ((next
